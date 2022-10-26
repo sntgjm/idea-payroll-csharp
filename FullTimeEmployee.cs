b4c9591a74fcc460a8b1ed8751a778e5bf888081
sntgjm <jm2001santiago@gmail.com>
2022-10-26 16:02:18 +0800

    payroll updated

---
+++ FullTimeEmployee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//sntgjm
+namespace payroll_github
+{
+    internal class FullTimeEmployee
+    {
+        public double monthSalary;
+
+        public void setMonthlySalary()
+        {
+            monthSalary = double.Parse(Console.ReadLine());    
+        }
+    }
+}
---
+++ PartTimeEmployee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//sntgjm
+namespace payroll_github
+{
+    internal class PartTimeEmployee
+    {
+        public double ratePerHour;
+        public int ratePerWorked;
+
+        public void setRatePerHour() {
+
+            ratePerHour = double.Parse(Console.ReadLine());
+
+        }
+
+        public void setRatePerWorked() {
+
+            ratePerWorked = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
